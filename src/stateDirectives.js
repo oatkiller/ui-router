b62d25d9f1c6e6b35bcb397ef44c89b056011338
oatkiller <robert@oatlab.com>
2015-03-04 15:35:01 -0500

    fix it

--- src/stateDirectives.js
+++ src/stateDirectives.js
@@ -84,68 +84,79 @@ function $StateRefDirective($state, $timeout) {
     restrict: 'A',
     require: ['?^uiSrefActive', '?^uiSrefActiveEq'],
     link: function(scope, element, attrs, uiSrefActive) {
-      var ref = parseStateRef(attrs.uiSref, $state.current.name);
-      var params = null, url = null, base = stateContext(element) || $state.$current;
-      // SVGAElement does not use the href attribute, but rather the 'xlinkHref' attribute.
-      var hrefKind = toString.call(element.prop('href')) === '[object SVGAnimatedString]' ?
-                 'xlink:href' : 'href';
-      var newHref = null, isAnchor = element.prop("tagName").toUpperCase() === "A";
-      var isForm = element[0].nodeName === "FORM";
-      var attr = isForm ? "action" : hrefKind, nav = true;
+      var watchers = [];
+      var processAttrs = function () {
+        // unbind any watchers
+        angular.forEach(function (watcher) {
+          watcher();
+        });
 
-      var options = { relative: base, inherit: true };
-      var optionsOverride = scope.$eval(attrs.uiSrefOpts) || {};
+        var ref = parseStateRef(attrs.uiSref, $state.current.name);
+        var params = null, url = null, base = stateContext(element) || $state.$current;
+        // SVGAElement does not use the href attribute, but rather the 'xlinkHref' attribute.
+        var hrefKind = toString.call(element.prop('href')) === '[object SVGAnimatedString]' ?
+                   'xlink:href' : 'href';
+        var newHref = null, isAnchor = element.prop("tagName").toUpperCase() === "A";
+        var isForm = element[0].nodeName === "FORM";
+        var attr = isForm ? "action" : hrefKind, nav = true;
 
-      angular.forEach(allowedOptions, function(option) {
-        if (option in optionsOverride) {
-          options[option] = optionsOverride[option];
+        var options = { relative: base, inherit: true };
+        var optionsOverride = scope.$eval(attrs.uiSrefOpts) || {};
+
+        angular.forEach(allowedOptions, function(option) {
+          if (option in optionsOverride) {
+            options[option] = optionsOverride[option];
+          }
+        });
+
+        var update = function(newVal) {
+          if (newVal) params = angular.copy(newVal);
+          if (!nav) return;
+
+          newHref = $state.href(ref.state, params, options);
+
+          var activeDirective = uiSrefActive[1] || uiSrefActive[0];
+          if (activeDirective) {
+            activeDirective.$$setStateInfo(ref.state, params);
+          }
+          if (newHref === null) {
+            nav = false;
+            return false;
+          }
+          attrs.$set(attr, newHref);
+        };
+
+        if (ref.paramExpr) {
+          watchers.push(scope.$watch(ref.paramExpr, function(newVal, oldVal) {
+            if (newVal !== params) update(newVal);
+          }, true));
+          params = angular.copy(scope.$eval(ref.paramExpr));
         }
-      });
+        update();
 
-      var update = function(newVal) {
-        if (newVal) params = angular.copy(newVal);
-        if (!nav) return;
+        if (isForm) return;
 
-        newHref = $state.href(ref.state, params, options);
+        element.bind("click", function(e) {
+          var button = e.which || e.button;
+          if ( !(button > 1 || e.ctrlKey || e.metaKey || e.shiftKey || element.attr('target')) ) {
+            // HACK: This is to allow ng-clicks to be processed before the transition is initiated:
+            var transition = $timeout(function() {
+              $state.go(ref.state, params, options);
+            });
+            e.preventDefault();
 
-        var activeDirective = uiSrefActive[1] || uiSrefActive[0];
-        if (activeDirective) {
-          activeDirective.$$setStateInfo(ref.state, params);
-        }
-        if (newHref === null) {
-          nav = false;
-          return false;
-        }
-        attrs.$set(attr, newHref);
+            // if the state has no URL, ignore one preventDefault from the <a> directive.
+            var ignorePreventDefaultCount = isAnchor && !newHref ? 1: 0;
+            e.preventDefault = function() {
+              if (ignorePreventDefaultCount-- <= 0)
+                $timeout.cancel(transition);
+            };
+          }
+        });
       };
-
-      if (ref.paramExpr) {
-        scope.$watch(ref.paramExpr, function(newVal, oldVal) {
-          if (newVal !== params) update(newVal);
-        }, true);
-        params = angular.copy(scope.$eval(ref.paramExpr));
-      }
-      update();
-
-      if (isForm) return;
-
-      element.bind("click", function(e) {
-        var button = e.which || e.button;
-        if ( !(button > 1 || e.ctrlKey || e.metaKey || e.shiftKey || element.attr('target')) ) {
-          // HACK: This is to allow ng-clicks to be processed before the transition is initiated:
-          var transition = $timeout(function() {
-            $state.go(ref.state, params, options);
-          });
-          e.preventDefault();
-
-          // if the state has no URL, ignore one preventDefault from the <a> directive.
-          var ignorePreventDefaultCount = isAnchor && !newHref ? 1: 0;
-          e.preventDefault = function() {
-            if (ignorePreventDefaultCount-- <= 0)
-              $timeout.cancel(transition);
-          };
-        }
-      });
+      attrs.$observe("uiSref",processAttrs);
+      attrs.$observe("uiSrefOpts",processAttrs);
+      processAttrs();
     }
   };
 }
